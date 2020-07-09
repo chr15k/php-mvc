9c3ebc1a7bdd038c44e41ea782cfa69bd840facf
chr15k <christhecoder85@gmail.com>
2020-07-09 12:20:30 +0100

    add error handling

---
+++ Core/Bootstrap/HandleExceptions.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Chr15k\Core\Bootstrap;
+
+use ErrorException;
+use Chr15k\Core\View\View;
+
+class HandleExceptions
+{
+    /**
+     * Bootstrap error handling.
+     *
+     * @return void
+     */
+    public function bootstrap()
+    {
+        error_reporting(-1);
+
+        set_error_handler([$this, 'handleError']);
+
+        set_exception_handler([$this, 'handleException']);
+
+        if (! config('app')['debug']) {
+            ini_set('display_errors', 'Off');
+        }
+    }
+
+    /**
+     * Convert PHP errors to ErrorException instances.
+     *
+     * @param  int  $level
+     * @param  string  $message
+     * @param  string  $file
+     * @param  int  $line
+     * @param  array  $context
+     * @return void
+     *
+     * @throws \ErrorException
+     */
+    public function handleError($level, $message, $file = '', $line = 0, $context = [])
+    {
+        if (error_reporting() & $level) {
+            throw new ErrorException($message, 0, $level, $file, $line);
+        }
+    }
+
+    /**
+     * Handle an uncaught exception from the application.
+     *
+     * Note: Most exceptions can be handled via the try / catch block in
+     * the HTTP and Console kernels. But, fatal error exceptions must
+     * be handled differently since they are not normal exceptions.
+     *
+     * @param  \Exception  $e
+     * @return void
+     */
+    public function handleException($e)
+    {
+        $code = $e->getCode();
+
+        if ($code !== 404) {
+            $code = 500;
+        }
+
+        http_response_code($code);
+
+        if (config('app')['debug']) {
+            echo "<h1>Fatal error</h1>";
+            echo "<p>Uncaught exception: '" . get_class($e) . "'</p>";
+            echo "<p>Message: '" . $e->getMessage() . "'</p>";
+            echo "<p>Stack trace:<pre>" . $e->getTraceAsString() . "</pre></p>";
+            echo "<p>Thrown in '" . $e->getFile() . "' on line " . $e->getLine() . "</p>";
+        } else {
+            $log = root_path() . '/logs/' . date('Y-m-d') . '.txt';
+            ini_set('error_log', $log);
+
+            $message = "Uncaught exception: '" . get_class($e) . "'";
+            $message .= " with message '" . $e->getMessage() . "'";
+            $message .= "\nStack trace: " . $e->getTraceAsString();
+            $message .= "\nThrown in '" . $e->getFile() . "' on line " . $e->getLine();
+
+            error_log($message);
+
+            View::make("$code.html");
+        }
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,9 +1,34 @@
 <?php
 
+define('PHP_MVC_START', microtime(true));
+
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+*/
 require __DIR__ . '/../vendor/autoload.php';
 
+/*
+|--------------------------------------------------------------------------
+| Load environment variables
+|--------------------------------------------------------------------------
+*/
 \Chr15k\Core\Support\Env::loadDotenv();
 
+/*
+|--------------------------------------------------------------------------
+| Bootstrap error handling
+|--------------------------------------------------------------------------
+*/
+$errorHandling = new \Chr15k\Core\Bootstrap\HandleExceptions();
+$errorHandling->bootstrap();
+
+/*
+|--------------------------------------------------------------------------
+| Register application routes
+|--------------------------------------------------------------------------
+*/
 $router = new \Chr15k\Core\Routing\Router();
 
 $router->register('/', [
@@ -12,4 +37,5 @@ $router->register('/', [
 ]);
 
 $request = new \Chr15k\Core\Http\Request();
+
 $router->dispatch($request);
